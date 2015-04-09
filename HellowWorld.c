398059a435c6f84724cac00814284a9810fd14c2
AgBasilio <agustinbasilio@gmail.com>
2015-04-08 23:48:07 -0300

    Update HellowWorld.c
    Se agrega el codigo del programa al archivo HellowWorld.c

--- HellowWorld.c
+++ HellowWorld.c
@@ -1,1 +1,14 @@
+/* Programa que muestra el mensaje "Hello Agustin!"
+ * Basilio Agustin Omar
+ * 08/04/2014
+ */
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Agustin!!" << endl;
+    return 0;
+}
